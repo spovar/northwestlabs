125b60ad0454c0fd9de77d958f2390f7cf5749e9
alecbrettbawden <alecbrettbawden@gmail.com>
2018-12-04 13:37:31 -0700

    created Assay Model and Test_assay model

---
+++ NorthwestLabs/Models/Assay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwestLabs.Models
+{
+    [Table("Assay")]
+    public class Assay
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int AssayID { get; set; }
+
+        public string assayDescription { get; set; }
+    }
+}
---
+++ NorthwestLabs/Models/Test_Assay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    public class Test_Assay
+    {
+        public int AssayID;
+        public Assay Assays = new Assay();
+
+        public int TestID;
+        public Test Tests = new Tests();
+        public bool? required;
+    }
+}
