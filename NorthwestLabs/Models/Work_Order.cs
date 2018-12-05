d55153f5b7d1385900b177a06668f8f84c2db26f
spovar <41087939+spovar@users.noreply.github.com>
2018-12-04 17:43:19 -0700

    added work orders models

---
+++ NorthwestLabs/Models/Work_Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    public class Work_Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public int LTNumber { get; set; }
+        public string Status { get; set; }
+        public DateTime dateArrived { get; set; }
+        public DateTime dateDue { get; set; }
+        public double custWeight { get; set; }
+        public double actualWeight { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
