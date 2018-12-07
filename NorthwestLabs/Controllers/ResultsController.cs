da48ba04ce717e8ae9cff19b816307045e2e95aa
Kevin Nelson <kevin.nelson0000@gmail.com>
2018-12-06 19:10:54 -0700

    Added Lab Data View

--- NorthwestLabs/Controllers/ResultsController.cs
+++ NorthwestLabs/Controllers/ResultsController.cs
@@ -30,6 +30,29 @@ namespace NorthwestLabs.Controllers
             }
             return View(resultslist);
         }
+        
+        public ActionResult labData()
+        {
+            List<Work_Order> compoundList = db.Work_Order.ToList();
+
+            foreach (var c2 in compoundList)
+            {
+                c2.compounds = db.Database.SqlQuery<Compounds>("SELECT DISTINCT Compounds.LTNumber, Compounds.compoundName FROM Compounds INNER JOIN Work_Order ON Compounds.LTNumber = Work_Order.LTNumber WHERE Work_Order.LTNumber = " + c2.LTNumber);
+            }    
+            
+            return View(compoundList);
+        }
+
+
+        [HttpPost]
+        public ActionResult labData(float weight)
+        {
+            db.Database.SqlQuery<Work_Order>("SELECT actualWeight FROM Work_Order");
+
+
+
+            return View();
+        }
 
         public ActionResult Index()
         {
